c832e3043fe65e6d790db73dd7b1dee80607ced5
PAO1 <pao1@Hackintosh-Mik.local>
2021-12-24 08:53:01 +0100

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default {
+  target: 'static', // default is 'server'
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - vue-stripe-checkout-nuxt-demo',
