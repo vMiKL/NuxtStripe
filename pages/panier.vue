d8a18086737f48c1819abac51c7ed28396339e97
PAO1 <pao1@Hackintosh-Mik.local>
2021-12-27 16:28:48 +0100

    upgrade

---
+++ pages/panier.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <stripe-element-card
+      ref="elementRef"
+      :pk="publishableKey"
+      @token="tokenCreated"
+    />
+    <button @click="submit">Generate token</button>
+  </div>
+</template>
+
+<script>
+import { StripeElementCard } from '@vue-stripe/vue-stripe';
+export default {
+  components: {
+    StripeElementCard,
+  },
+  data () {
+    this.publishableKey = 'pk_test_KRUvZiEQgpnKHiwV9rc88RjE';
+    return {
+      token: null,
+    };
+  },
+  method: {
+    submit () {
+      // this will trigger the process
+      this.$refs.elementRef.submit();
+    },
+    tokenCreated (token) {
+      console.log(token);
+      // handle the token
+      // send it to your server
+    },
+  },
+};
+</script>
