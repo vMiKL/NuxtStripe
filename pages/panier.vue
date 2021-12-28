af398cdefb5048e24937fbc833db1f2f0411ac96
PAO1 <pao1@Hackintosh-Mik.local>
2021-12-28 09:15:19 +0100

    upgrade

--- pages/panier.vue
+++ pages/panier.vue
@@ -1,36 +1,88 @@
-<template>
-  <div>
-    <stripe-element-card
-      ref="elementRef"
-      :pk="publishableKey"
-      @token="tokenCreated"
-    />
-    <button @click="submit">Generate token</button>
-  </div>
+<template lang="pug">
+  v-container
+    v-row(justify="center").mt-16
+      v-col(cols="12" md="6")
+        v-card(outlined)
+          v-card-text.text-center
+            img(width="200" src="../assets/common-sense.jpeg")
+            h2 Buy common sense for $10.000
+            stripe-checkout(
+              ref="checkoutRef"
+              mode="payment"
+              :pk="pk"
+              :line-items="lineItems"
+              :success-url="successURL"
+              :cancel-url="cancelURL"
+              @loading="v => loading = v"
+            )
+            br
+            v-alert(
+              dark
+              v-if="redirectState"
+              :color="redirectState"
+              dismissible
+            ) Payment {{redirectState}}
+          v-card-actions
+            v-spacer
+            v-btn(
+              depressed
+              large
+              color="primary"
+              :loading="loading"
+              :disabled="loading"
+              @click="checkout"
+            ).text-none Pay $10
 </template>
 
 <script>
-import { StripeElementCard } from '@vue-stripe/vue-stripe';
+import headMeta from '~/utils/head-meta';
+
 export default {
-  components: {
-    StripeElementCard,
-  },
   data () {
-    this.publishableKey = 'pk_test_KRUvZiEQgpnKHiwV9rc88RjE';
+    this.pk = 'pk_test_KRUvZiEQgpnKHiwV9rc88RjE';
     return {
+      loading: false,
+      lineItems: [
+        {
+          price: 'price_1KBKLLLdPoxuSz0o2bEbH3kT', // L'identifiant du prix unique que vous avez créé dans votre tableau de bord Stripe, création d'un article
+          quantity: 1,
+        },
+      ],
       token: null,
+      successURL: process.client && `${window.location.origin}${window.location.pathname}?state=success`,
+      cancelURL: process.client && `${window.location.origin}${window.location.pathname}?state=error`,
+      // Misc
+      redirectState: '',
     };
   },
-  method: {
-    submit () {
-      // this will trigger the process
-      this.$refs.elementRef.submit();
+  computed: {
+    queryState () {
+      return process.client && (new URLSearchParams(window.location.search).get('state') || '');
+    },
+  },
+  mounted () {
+    if (this.queryState === 'success') {
+      this.redirectState = 'success';
+    }
+    if (this.queryState === 'error') {
+      this.redirectState = 'error';
+    }
+  },
+  methods: {
+    checkout () {
+      this.loading = true;
+      this.$refs.checkoutRef.redirectToCheckout();
     },
     tokenCreated (token) {
-      console.log(token);
-      // handle the token
-      // send it to your server
+      this.token = token;
     },
+  },
+  head () {
+    return headMeta({
+      title: 'Vue Stripe + Nuxt.js Demo',
+      description: 'A demo on how to implement Vue Stripe in Nuxt.js',
+      socialBanner: require('../assets/vue-stripe-logo-variant-1-small.png'),
+    });
   },
 };
 </script>
